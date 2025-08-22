b4b217f18090297bcf7626bc82575300225421b2
Tim Pritlove <tim@pritlove.org>
2025-08-22 16:49:56 +0200

    create artist resource

---
+++ lib/tunez/music/artist.ex
@@ -0,0 +1,19 @@
+defmodule Tunez.Music.Artist do
+  use Ash.Resource, otp_app: :tunez, domain: Tunez.Music, data_layer: AshPostgres.DataLayer
+
+  postgres do
+    table "artists"
+    repo Tunez.Repo
+  end
+
+  attributes do
+    uuid_primary_key :id
+    attribute :name, :string do
+      allow_nil? false
+    end
+    attribute :biography, :string
+
+    create_timestamp :created_at
+    update_timestamp :updated_at
+  end
+end
