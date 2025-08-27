433c56c22278cce112f544b9c4c7dd92eac7b911
Tim Pritlove <tim@pritlove.org>
2025-08-27 19:22:16 +0200

    fix typo

--- mix.exs
+++ mix.exs
@@ -80,8 +80,8 @@ defmodule Tunez.MixProject do
       setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
       "ecto.setup": ["ecto.create", "ecto.migrate"],
       seed: [
-        "run priv/repo/seeds/01-artists.exs"
-        "run priv/repo/seeds/02-albums.exs",
+        "run priv/repo/seeds/01-artists.exs",
+        "run priv/repo/seeds/02-albums.exs"
         # "run priv/repo/seeds/08-tracks.exs"
       ],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
