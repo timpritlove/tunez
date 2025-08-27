9bc8b11c71a44e5a2b95cacc68280f75938f33eb
Tim Pritlove <tim@pritlove.org>
2025-08-27 18:45:38 +0200

    create album resource

---
+++ lib/tunez/music/album.ex
@@ -0,0 +1,24 @@
+defmodule Tunez.Music.Album do
+  use Ash.Resource, otp_app: :tunez, domain: Tunez.Music, data_layer: AshPostgres.DataLayer
+
+  postgres do
+    table "albums"
+    repo Tunez.Repo
+  end
+
+  attributes do
+    uuid_primary_key :id
+
+    attribute :name, :string do
+      allow_nil? false
+    end
+
+    attribute :year_released, :integer do
+      allow_nil? false
+    end
+
+    attribute :cover_image_url, :string
+    create_timestamp :inserted_at
+    update_timestamp :updated_at
+  end
+end
