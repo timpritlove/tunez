bca3234003007489e5560eb2c9c29aa2c104ae04
Tim Pritlove <tim@pritlove.org>
2025-08-28 02:13:06 +0200

    defining an inline change (store previous_names for artists)

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -17,6 +17,9 @@ defmodule Tunez.Music.Artist do
     attribute :name, :string do
       allow_nil? false
     end
+    attribute :previous_names, {:array, :string} do
+      default []
+    end
     attribute :biography, :string
 
     create_timestamp :created_at
@@ -34,6 +37,14 @@ defmodule Tunez.Music.Artist do
 
     update :update do
       accept [:name, :biography]
+      require_atomic? false
+      change fn changeset, _context ->
+        new_name = Ash.Changeset.get_attribute(changeset, :name)
+        previous_name = Ash.Changeset.get_data(changeset, :name)
+        previous_names = Ash.Changeset.get_data(changeset, :previous_names)
+        names = [previous_name | previous_names] |> Enum.uniq() |> Enum.reject(fn name -> name == new_name end)
+        Ash.Changeset.change_attribute(changeset, :previous_names, names)
+      end, where: [changing(:name)]
     end
 
     destroy :destroy do
