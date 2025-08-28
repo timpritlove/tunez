02214884c40be4c21ba3c78178ff28c724a40ecf
Tim Pritlove <tim@pritlove.org>
2025-08-28 02:28:44 +0200

    turn change function into a change module

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -38,13 +38,7 @@ defmodule Tunez.Music.Artist do
     update :update do
       accept [:name, :biography]
       require_atomic? false
-      change fn changeset, _context ->
-        new_name = Ash.Changeset.get_attribute(changeset, :name)
-        previous_name = Ash.Changeset.get_data(changeset, :name)
-        previous_names = Ash.Changeset.get_data(changeset, :previous_names)
-        names = [previous_name | previous_names] |> Enum.uniq() |> Enum.reject(fn name -> name == new_name end)
-        Ash.Changeset.change_attribute(changeset, :previous_names, names)
-      end, where: [changing(:name)]
+      change Tunez.Music.Changes.UpdatePreviousNames, where: [changing(:name)]
     end
 
     destroy :destroy do
