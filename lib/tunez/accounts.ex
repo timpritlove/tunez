2972105d3340d4726cd78bdeb8d20f34df98b4c5
Tim Pritlove <tim@pritlove.org>
2025-08-31 17:08:42 +0200

    setup GraphQL for Accounts

--- lib/tunez/accounts.ex
+++ lib/tunez/accounts.ex
@@ -1,28 +1,43 @@
 defmodule Tunez.Accounts do
-  use Ash.Domain, otp_app: :tunez, extensions: [AshJsonApi.Domain]
+  use Ash.Domain, otp_app: :tunez, extensions: [AshGraphql.Domain, AshJsonApi.Domain]
 
-  resources do
-    resource Tunez.Accounts.Token
-    resource Tunez.Accounts.User
+  graphql do
+    mutations do
+      create Tunez.Accounts.User, :register_user, :register_with_password
+    end
+
+    queries do
+      get Tunez.Accounts.User, :sign_in_user, :sign_in_with_password do
+        identity false
+        type_name :user_with_token
+      end
+    end
   end
 
   json_api do
     routes do
       base_route "/users", Tunez.Accounts.User do
         post :register_with_password do
-           route "/register"
-           metadata fn _subject, user, _request ->
+          route "/register"
+
+          metadata fn _subject, user, _request ->
             %{token: user.__metadata__.token}
-           end
+          end
         end
 
         post :sign_in_with_password do
           route "/sign_in"
+
           metadata fn _subject, user, _request ->
             %{token: user.__metadata__.token}
           end
         end
       end
     end
+  end
+
+  resources do
+    resource Tunez.Accounts.Token
+    resource Tunez.Accounts.User
   end
 end
