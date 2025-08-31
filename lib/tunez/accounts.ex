2923d77ff78039eeb2827598e91cf48c7db92459
Tim Pritlove <tim@pritlove.org>
2025-08-31 16:47:39 +0200

    enable account creation and sign-in via JSON API

--- lib/tunez/accounts.ex
+++ lib/tunez/accounts.ex
@@ -5,4 +5,24 @@ defmodule Tunez.Accounts do
     resource Tunez.Accounts.Token
     resource Tunez.Accounts.User
   end
+
+  json_api do
+    routes do
+      base_route "/users", Tunez.Accounts.User do
+        post :register_with_password do
+           route "/register"
+           metadata fn _subject, user, _request ->
+            %{token: user.__metadata__.token}
+           end
+        end
+
+        post :sign_in_with_password do
+          route "/sign_in"
+          metadata fn _subject, user, _request ->
+            %{token: user.__metadata__.token}
+          end
+        end
+      end
+    end
+  end
 end
