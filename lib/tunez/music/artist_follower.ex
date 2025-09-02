18024f61adbba6bba6588240d6152e9a36eaec19
Tim Pritlove <tim@pritlove.org>
2025-09-03 01:48:32 +0200

    Using ArtistFollower to Link Artists and Users

--- lib/tunez/music/artist_follower.ex
+++ lib/tunez/music/artist_follower.ex
@@ -1,5 +1,9 @@
 defmodule Tunez.Music.ArtistFollower do
-  use Ash.Resource, otp_app: :tunez, domain: Tunez.Music, data_layer: AshPostgres.DataLayer
+  use Ash.Resource,
+    otp_app: :tunez,
+    domain: Tunez.Music,
+    data_layer: AshPostgres.DataLayer,
+    authorizers: [Ash.Policy.Authorizer]
 
   postgres do
     table "artist_followers"
@@ -8,6 +12,16 @@ defmodule Tunez.Music.ArtistFollower do
     references do
       reference :artist, on_delete: :delete, index?: true
       reference :follower, on_delete: :delete
+    end
+  end
+
+  actions do
+    defaults [:read]
+  end
+
+  policies do
+    policy action_type(:read) do
+      authorize_if always()
     end
   end
 
