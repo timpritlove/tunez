3d3353699dfc9a184c0475d0d2d1b8b98c586d49
Tim Pritlove <tim@pritlove.org>
2025-09-02 19:34:37 +0200

    enable track seeding

--- mix.exs
+++ mix.exs
@@ -91,8 +91,8 @@ defmodule Tunez.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate"],
       seed: [
         "run priv/repo/seeds/01-artists.exs",
-        "run priv/repo/seeds/02-albums.exs"
-        # "run priv/repo/seeds/08-tracks.exs"
+        "run priv/repo/seeds/02-albums.exs",
+        "run priv/repo/seeds/08-tracks.exs"
       ],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
